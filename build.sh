df646ea993c98c342d2233de9e62c4760439316d
Max Ogden <max@maxogden.com>
2015-08-30 12:32:12 -0700

    use corepure

--- build.sh
+++ build.sh
@@ -1,38 +1,37 @@
 # create a tinycorelinux fs with custom .tcz packages
 # prerequisites: linux, apt-get install squashfs-tools, npm i nugget -g
 
+export TCL_SERVER=http://tinycorelinux.net/6.x/x86_64
+
+mkdir -p downloads dist
+
 # dl release + packages (add your packages here)
-nugget http://tinycorelinux.net/6.x/x86_64/release/CorePure64-6.3.iso \
-	https://github.com/mafintosh/hyperfused/releases/download/0.0.0/hyperfused.tcz \
-	http://tinycorelinux.net/6.x/x86_64/tcz/fuse.tcz \	
-	-c
+nugget -c -d downloads \
+  $TCL_SERVER/release/distribution_files/corepure64.gz \
+  $TCL_SERVER/tcz/fuse.tcz \
+  https://github.com/mafintosh/hyperfused/releases/download/0.0.0/hyperfused.tcz
 
 # install packages
-unsquashfs -f fuse.tcz
-unsquashfs -f hyperfused.tcz
+unsquashfs -f -d dist downloads/fuse.tcz
+unsquashfs -f -d dist downloads/hyperfused.tcz
 
-# extract kernel, fs files from iso
-sudo mkdir -p /mnt/tmp
-sudo mount CorePure64-6.3.iso /mnt/tmp -o loop,ro
-cp /mnt/tmp/boot/vmlinuz64 vmlinuz
-cp /mnt/tmp/boot/corepure64.gz core.gz
-sudo umount /mnt/tmp
-sudo rm -rf /mnt/tmp
-
-# extract core
-mkdir -p core
-( cd core ; zcat ../core.gz | sudo cpio -idm )
-
-# copy extracted packages into fs
-sudo cp -Rp squashfs-root/usr/ core/
+# extract rootfs
+cd dist
+zcat ../downloads/corepure64.gz | sudo cpio -f -i -H newc -d --no-absolute-filenames
 
 # enables terminal (i think, blindly copied from xhyve example)
-sudo sed -i '/^# ttyS0$/s#^..##' core/etc/securetty
-sudo sed -i '/^tty1:/s#tty1#ttyS0#g' core/etc/inittab
+sudo sed -i '/^# ttyS0$/s#^..##' etc/securetty
+sudo sed -i '/^tty1:/s#tty1#ttyS0#g' etc/inittab
+
+cd ../
+
+# copy our files in
+sudo rsync --recursive include/ dist
 
 # repackage core into output.gz
-( cd core ; find | sudo cpio -o -H newc ) | gzip -c > hypercore.gz
+(cd dist ; find | sudo cpio -o -H newc) | gzip -c > hypercore.gz
 
 # cleanup
-sudo rm -rf squashfs-root/ core/
-# now boot vmlinuz and output.gz like this https://github.com/mist64/xhyve/blob/cd782515fff03bd4b80da60e29108f6b33476bf5/xhyverun.sh
+sudo rm -rf dist
+
+# now boot vmlinuz64 and output.gz like this https://github.com/mist64/xhyve/blob/cd782515fff03bd4b80da60e29108f6b33476bf5/xhyverun.sh
